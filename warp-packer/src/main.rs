7c110e143881e1ef63eb2a92e7f9cf4a1986c5e1
Nik V <github@nikvdp.com>
2021-11-14 12:59:33 +0800

    disable aarch64 builds

--- warp-packer/src/main.rs
+++ warp-packer/src/main.rs
@@ -33,7 +33,7 @@ const RUNNER_EXEC_MAGIC: &[u8] = b"tVQhhsFFlGGD3oWV4lEPST8I8FEPP54IM0q7daes4E1y3
 const RUNNER_UID_MAGIC: &[u8] = b"DR1PWsJsM6KxNbng9Y38\0";
 
 const RUNNER_LINUX_X64: &[u8] = include_bytes!("../../target/x86_64-unknown-linux-gnu/release/warp-runner");
-const RUNNER_LINUX_AARCH64: &[u8] = include_bytes!("../../target/aarch64-unknown-linux-gnu/release/warp-runner");
+// const RUNNER_LINUX_AARCH64: &[u8] = include_bytes!("../../target/aarch64-unknown-linux-gnu/release/warp-runner");
 const RUNNER_MACOS_X64: &[u8] = include_bytes!("../../target/x86_64-apple-darwin/release/warp-runner");
 const RUNNER_WINDOWS_X86: &[u8] = include_bytes!("../../target/i686-pc-windows-msvc/release/warp-runner.exe");
 const RUNNER_WINDOWS_X64: &[u8] = include_bytes!("../../target/x86_64-pc-windows-msvc/release/warp-runner.exe");
@@ -42,7 +42,7 @@ lazy_static! {
     static ref RUNNER_BY_ARCH: HashMap<&'static str, &'static [u8]> = {
         let mut m = HashMap::new();
         m.insert("linux-x64", RUNNER_LINUX_X64);
-        m.insert("linux-aarch64", RUNNER_LINUX_AARCH64);
+        // m.insert("linux-aarch64", RUNNER_LINUX_AARCH64);
         m.insert("macos-x64", RUNNER_MACOS_X64);
         m.insert("windows-x86", RUNNER_WINDOWS_X86);
         m.insert("windows-x64", RUNNER_WINDOWS_X64);
@@ -276,3 +276,5 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("All done");
     Ok(())
 }
+
+
